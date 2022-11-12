07aed9dd3bec3820d984467f0711062991586799
manishtmtmt <manisht.mt.mt@gmail.com>
2022-11-12 22:03:01 +0530

    initialized firebase project

---
+++ src/misc/firebase.config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyD2dL96se3yIXIPKOtvtTuPU4vPcmYknf8",
+  authDomain: "chat-app-15798.firebaseapp.com",
+  projectId: "chat-app-15798",
+  storageBucket: "chat-app-15798.appspot.com",
+  messagingSenderId: "159523893460",
+  appId: "1:159523893460:web:068a680501040583488b41",
+};
+
+const app = firebase.initializeApp(config);
