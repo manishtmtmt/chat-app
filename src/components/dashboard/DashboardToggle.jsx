3bd1a879a443bd8a70e717b20142d71a8c4a9ac5
manishtmtmt <manisht.mt.mt@gmail.com>
2022-11-13 11:42:00 +0530

    started building dashboard; add signout button and display user name

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { Button, Drawer } from "rsuite";
+import React, { useCallback } from "react";
+import { Button, Drawer, Message, toaster } from "rsuite";
 import DashboardIcon from "@rsuite/icons/Dashboard";
 import { useMediaQuery, useModalState } from "../../misc/custom-hooks";
 import Dashboard from ".";
+import { auth } from "../../misc/firebase.config";
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery("(max-width: 992px)");
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+
+    toaster.push(
+      <Message type="info" closable duration={4000}>
+        Signed Out
+      </Message>
+    );
+
+    close();
+  }, [close]);
 
   return (
     <>
@@ -19,7 +32,7 @@ const DashboardToggle = () => {
         onClose={close}
         placement="left"
       >
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Profiler } from "react";
+import { Button, Drawer } from "rsuite";
+import { useProfile } from "../../context/profile.context";
 
-const Dashboard = () => {
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
   return (
-    <div>Hello</div>
-  )
-}
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
 
-export default Dashboard
+      <Drawer.Body>
+        <div style={{ height: "90%" }}>
+          <h3>Hey, {profile.name}</h3>
+        </div>
+        <div style={{ height: "10%" }}>
+          <Drawer.Actions>
+            <Button block color="red" appearance="primary" onClick={onSignOut}>
+              Sign Out
+            </Button>
+          </Drawer.Actions>
+        </div>
+      </Drawer.Body>
+    </>
+  );
+};
+
+export default Dashboard;
