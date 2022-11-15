7e3e0ecdb234b3613720831592c6806cf2ff7e0e
manishtmtmt <manisht.mt.mt@gmail.com>
2022-11-15 19:19:03 +0530

    added an option to record an audio file

--- src/components/chat_window/bottom/AudioMsgBtn.jsx
+++ src/components/chat_window/bottom/AudioMsgBtn.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
+import { InputGroup, Message, toaster } from "rsuite";
+import { ReactMic } from "react-mic";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "../../../misc/firebase.config";
 
-const AudioMsgBtn = () => {
+const AudioMsgBtn = ({ afterUpload }) => {
+  const { chatId } = window;
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const onClick = useCallback(() => {
+    setIsRecording((p) => !p);
+  }, []);
+
+  const onUpload = useCallback(
+    async (data) => {
+      setIsUploading(true);
+      try {
+        const snap = await uploadBytes(
+          ref(storage, `/chat/${chatId}/audio_${Date.now()}.mp3`),
+          data.blob,
+          {
+            cacheControl: `public, max-age=${3600 * 24 * 3}`,
+          }
+        );
+
+        const file = {
+          contentType: snap.metadata.contentType,
+          name: snap.metadata.name,
+          url: await getDownloadURL(snap.ref),
+        };
+
+        setIsUploading(false);
+        afterUpload([file]);
+      } catch (error) {
+        setIsUploading(false);
+        toaster.push(
+          <Message type="error" closable duration={4000}>{error.message}</Message>
+        )
+      }
+    },
+    [afterUpload, chatId]
+  );
+
   return (
-    <div>AudioMsgBtn</div>
-  )
-}
+    <InputGroup.Button
+      onClick={onClick}
+      disabled={isUploading}
+      className={isRecording ? "animate-blink" : ""}
+    >
+      <i className="fa-solid fa-microphone"></i>
+      <ReactMic
+        record={isRecording}
+        className="d-none"
+        onStop={onUpload}
+        mimeType="audio/mp3"
+      />
+    </InputGroup.Button>
+  );
+};
 
-export default AudioMsgBtn
+export default AudioMsgBtn;
--- src/components/chat_window/bottom/index.jsx
+++ src/components/chat_window/bottom/index.jsx
@@ -6,6 +6,7 @@ import { useParams } from "react-router";
 import { useProfile } from "../../../context/profile.context";
 import { database } from "../../../misc/firebase.config";
 import AttchmentBtnModal from "./AttchmentBtnModal";
+import AudioMsgBtn from "./AudioMsgBtn";
 
 function assembleMessage(profile, chatId) {
   return {
@@ -113,6 +114,7 @@ const ChatBottom = () => {
     <div>
       <InputGroup>
         <AttchmentBtnModal afterUpload={afterUpload} />
+        <AudioMsgBtn afterUpload={afterUpload} />
         <Input
           placeholder="Write a new message here..."
           value={input}
