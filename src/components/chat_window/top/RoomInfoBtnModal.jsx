70ad0b8d62f7f660d9f9b0b990271e4d32eaaf12
manishtmtmt <manisht.mt.mt@gmail.com>
2022-11-15 07:57:35 +0530

    started to build chat top; created room description modal

--- src/components/chat_window/top/RoomInfoBtnModal.jsx
+++ src/components/chat_window/top/RoomInfoBtnModal.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Button, Modal } from "rsuite";
+import { useCurrentRoom } from "../../../context/current-room.context";
+import { useModalState } from "../../../misc/custom-hooks";
 
 const RoomInfoBtnModal = () => {
-  return (
-    <div>RoomInfoBtnModal</div>
-  )
-}
+  const { isOpen, open, close } = useModalState();
+  const description = useCurrentRoom((v) => v.description);
+  const name = useCurrentRoom((v) => v.name);
 
-export default RoomInfoBtnModal
+  return (
+    <>
+      <Button appearance="link" className="px-0" onClick={open}>
+        Room information
+      </Button>
+
+      <Modal open={isOpen} onClose={close}>
+        <Modal.Header>
+          <Modal.Title>About {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6 className="mb-1">Description</h6>
+          <p>{description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
--- src/components/chat_window/top/index.jsx
+++ src/components/chat_window/top/index.jsx
@@ -1,10 +1,35 @@
 import React, { memo } from "react";
+import { Link } from "react-router-dom";
 import { useCurrentRoom } from "../../../context/current-room.context";
+import ArowBackIcon from '@rsuite/icons/ArowBack';
+import { useMediaQuery } from "../../../misc/custom-hooks";
+import { ButtonToolbar } from "rsuite";
+import RoomInfoBtnModal from "./RoomInfoBtnModal";
 
 const ChatTop = () => {
   const name = useCurrentRoom((v) => v.name);
+  const isMobile = useMediaQuery("(max-width: 992px)");
 
-  return <div>{name}</div>;
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Link to={"/"} className="link-unstyled">
+            <ArowBackIcon
+              className={isMobile ? "d-inline-block p-0 mr-2 text-blue" : "d-none"}
+            />
+          </Link>
+          <span className="text-disappear">{name}</span>
+        </h4>
+
+        <ButtonToolbar className="ws-nowrap">Todo</ButtonToolbar>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default memo(ChatTop);
